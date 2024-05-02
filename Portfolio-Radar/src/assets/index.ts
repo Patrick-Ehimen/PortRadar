cd6f1d12c4b8b596626733cf4e4d08c6ce1370a5
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-05-02 19:18:01 +0100

    added project's assests

---
+++ Portfolio-Radar/src/assets/index.ts
@@ -0,0 +1,151 @@
+import Logo from "./logo.svg";
+import closeMenu from "./closeMenu.svg";
+import Menu from "./Menu.svg";
+import HomeImg from "./homeImg.svg";
+import PlankImgOne from "./plankImgOne.svg";
+import PlankImgTwo from "./plankImgTwo.svg";
+import FeatureImg from "./feature-image.svg";
+import FeatureImgThree from "./feature-image3.svg";
+import FeatureImgFour from "./feature-image4.svg";
+import Arrow from "./arr.svg";
+import Arbitrum from "./arbitrum.svg";
+import Beamswap from "./beamswap.svg";
+import Busd from "./BUSD.svg";
+import BasiliskSnekSwap from "./BasiliskSnekSwap.svg";
+import Curve from "./Curve.svg";
+import Centrifuge from "./Centrifuge (CFG).svg";
+import DappRadar from "./DappRadar.svg";
+import KusamaLido from "./Kusama Lido.svg";
+import FraxFinance from "./Frax Finance (FRAX).svg";
+import Uniswap from "./Uniswap (UNI).svg";
+import USDT from "./USDT.svg";
+import Polygon from "./Polygon (MATIC).svg";
+import Polymesh from "./Polymesh (POLYX).svg";
+import WBTC from "./WBTC.svg";
+import DashboardIcon from "./app-assests/DashboardIcon.svg";
+import PriceIcon from "./app-assests/PriceIcon.svg";
+import ExhangeIcon from "./app-assests/ExchangeIcon.svg";
+import WalletIcon from "./app-assests/WalletIcon.svg";
+import NftMarkets from "./app-assests/NftMarket.svg";
+import NewsIcon from "./app-assests/NewsIcon.svg";
+import Swap from "./app-assests/Swap.svg";
+import StakeIcon from "./app-assests/StakeIcon.svg";
+import BridgeIcon from "./app-assests/BridgeIcon.svg";
+import DefaultAvatar from "./app-assests/Avatar.svg";
+import LightIcon from "./app-assests/LightIcon.svg";
+import DarkIcon from "./app-assests/DarkIcon.svg";
+import MegaMenue from "./app-assests/MegaMenue.svg";
+import NotificationIcon from "./app-assests/NotificationBell.svg";
+import SettingsIcon from "./app-assests/SettingsIcon.svg";
+import MarginTrade from "./app-assests/MarginTrade.svg";
+import MobileApp from "./app-assests/MobileApp.svg";
+import News from "./app-assests/News.svg";
+import TradeAPI from "./app-assests/TradeAPI.svg";
+import Wallet from "./app-assests/Wallet.svg";
+import Widgets from "./app-assests/Widget.svg";
+import Apple from "./app-assests/AppleIcon.svg";
+import Android from "./app-assests/AndroidIcon.svg";
+import Chinese from "./app-assests/Chinese.svg";
+import English from "./app-assests/English.svg";
+import French from "./app-assests/French.svg";
+import German from "./app-assests/German.svg";
+import Spanish from "./app-assests/Spainish.svg";
+import Sweedish from "./app-assests/Sweedish.svg";
+import LangOptions from "./app-assests/LangOptions.svg";
+import CloseLang from "./app-assests/CloseLang.svg";
+import ArrowIcon from "./app-assests/ArrowIcon.svg";
+import RightArrowIcon from "./app-assests/RightIcon.svg";
+import USD from "./app-assests/USD.svg";
+import WalletModal from "./app-assests/WalletModal.svg";
+import WhaleSlang from "./app-assests/WhaleSlang.svg";
+import WhaleSlangWhite from "./app-assests/whiteBg-Img/WhaleSlangWhite.svg";
+import Avalance from "./app-assests/Avalance.svg";
+import BinanceSmartChain from "./app-assests/BinanceSmartChain.svg";
+import Ethereum from "./app-assests/Ethereum.svg";
+import Optimism from "./app-assests/Optimism.svg";
+import Base from "./app-assests/Base.svg";
+import Sepolia from "./app-assests/Sepolia.svg";
+import Mumbai from "./app-assests/Mumbai.svg";
+import BalanceBackground from "./app-assests/BalanceBackground.svg";
+import StarIcon from "./app-assests/StarIcon.svg";
+import ArrUpDown from "./app-assests/ArrUpDown.svg";
+import Erc20Currency from "./app-assests/Erc20Currency.svg";
+import ExchangeSwap from "./app-assests/exchangeSwap.svg";
+import EmptyWallet from "./app-assests/EmptyWallet.svg";
+
+export {
+  Chinese,
+  English,
+  French,
+  German,
+  Spanish,
+  Sweedish,
+  LangOptions,
+  CloseLang,
+  ArrowIcon,
+  Logo,
+  closeMenu,
+  Menu,
+  HomeImg,
+  PlankImgOne,
+  PlankImgTwo,
+  FeatureImg,
+  FeatureImgThree,
+  FeatureImgFour,
+  Arrow,
+  KusamaLido,
+  Arbitrum,
+  Beamswap,
+  Busd,
+  BasiliskSnekSwap,
+  Curve,
+  Centrifuge,
+  DappRadar,
+  FraxFinance,
+  Uniswap,
+  USDT,
+  Polygon,
+  Polymesh,
+  WBTC,
+  DashboardIcon,
+  PriceIcon,
+  ExhangeIcon,
+  WalletIcon,
+  NftMarkets,
+  NewsIcon,
+  Swap,
+  StakeIcon,
+  BridgeIcon,
+  DefaultAvatar,
+  LightIcon,
+  DarkIcon,
+  MegaMenue,
+  NotificationIcon,
+  SettingsIcon,
+  MarginTrade,
+  MobileApp,
+  News,
+  TradeAPI,
+  Wallet,
+  Widgets,
+  Apple,
+  Android,
+  RightArrowIcon,
+  USD,
+  WalletModal,
+  WhaleSlang,
+  WhaleSlangWhite,
+  Avalance,
+  BinanceSmartChain,
+  Ethereum,
+  Optimism,
+  Base,
+  Sepolia,
+  Mumbai,
+  BalanceBackground,
+  StarIcon,
+  ArrUpDown,
+  Erc20Currency,
+  ExchangeSwap,
+  EmptyWallet,
+};
